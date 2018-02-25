c5557bae99ce41737e3610809ebf0776c2ee1298
Jungho Kim <jk@architech.ca>
2018-02-25 18:38:44 -0500

    added support for Conduit service-mesh

---
+++ service-mesh/README.md
@@ -0,0 +1,15 @@
+# Service-Mesh #
+
+## Deploying the Todo-App with Conduit ##
+
+[Conduit](https://conduit.io/getting-started/) is a lightweight service-mesh that was announced at the recent Kubecon.  It is an opensource project initiated by [Bouyant](https://buoyant.io/) the company behind the [Linkerd](https://linkerd.io/) service-mesh.  Read the excellent blog article ['What is a service-mesh and why do I need one?'](https://buoyant.io/2017/04/25/whats-a-service-mesh-and-why-do-i-need-one/) by Bouyant.
+
+To deploy the todo-app so that Conduit proxies requests to the todo-app pods:
+
+1. Install Conduit.  See the [Getting Started Guide](https://conduit.io/getting-started/)
+2. From within the [todo-app/manual-k8s-deploy](../todo-app/manual-k8s-deploy) directory, execute `./deploy-todo-app.sh [-n namespace] -c`
+
+## References ##
+
+- [Linkerd service-mesh](https://linkerd.io)
+- [Istio service-mesh](https://istio.io/)
--- todo-app/manual-k8s-deploy/deploy-todo-app.sh
+++ todo-app/manual-k8s-deploy/deploy-todo-app.sh
@@ -1,19 +1,50 @@
+#!/usr/bin/env bash
 
-if [ "$#" -ne 1 ] ; then
-  echo "Usage: $0 namespace" >&2
-  echo "assuming it is todo-app"
-  todoapp_namespace="todo-app"
-else
-  todoapp_namespace=$1
-fi
+deployWithConduit=false
+namespace='todo-app'
+
+function checkConduitInstalled {
+    if conduit version
+    then
+      echo "conduit is installed"
+    else
+      echo "conduit is not installed.  Please go to https://conduit.io/getting-started/ for instructions on how to install.";
+      exit 1;
+    fi
+}
+
+while getopts :cn: opt; do
+    case $opt in
+        c)
+            deployWithConduit=true
+            checkConduitInstalled
+            ;;
+        n)
+            namespace=${OPTARG:-'todo-app'}
+            ;;
+        \?) #invalid option
+            echo "usage: ./deploy-todo-app [-c] [-n namespace] default namespace is 'todo-app'"
+            exit 1
+            ;;
+    esac
+done
 
 #Create the secrets need by the application to access Azure AD
-kubectl create secret generic todoapp-secrets --from-file=secrets --namespace=$todoapp_namespace
+kubectl create secret generic todoapp-secrets --from-file=secrets --namespace="$namespace"
 
 #Create the configuration object for the todo and user APIs
-kubectl create --namespace=$todoapp_namespace -f todo-api-config.yaml
-kubectl create --namespace=$todoapp_namespace -f user-api-config.yaml
-kubectl create --namespace=$todoapp_namespace -f todo-ui-config.yaml
+kubectl create --namespace="$namespace" -f todo-api-config.yaml
+kubectl create --namespace="$namespace" -f user-api-config.yaml
+kubectl create --namespace="$namespace" -f todo-ui-config.yaml
 
 #Now create the application
-kubectl create --namespace=$todoapp_namespace -f todo-app.yaml
+
+if [ "$deployWithConduit" = true ]; then
+    echo "deploying todo-app to namespace: '$namespace' WITH the Conduit side proxy."
+    conduit inject todo-app.yaml | kubectl create --namespace="$namespace" -f -
+    echo ""
+    echo "access the Conduit Dashboard by executing 'conduit dashboard'"
+else
+    echo "deploying todo-app to namespace: '$namespace'"
+    kubectl create -f todo-app.yaml --namespace="$namespace"
+fi
