e336679218e324b0a7d03a172853f357f9aab34c
Jungho Kim <jk@architech.ca>
2018-04-14 16:36:05 -0400

    parameterize tag

--- services/random-quote-api/build.sh
+++ services/random-quote-api/build.sh
@@ -3,4 +3,4 @@
 #required to statically compile all dependencies into a single binary
 CGO_ENABLED=0 GOOS=linux go build -a -installsuffix cgo -o main main.go
 
-docker build --no-cache . -t architechbootcamp/random-quote:1.0.0
+docker build --no-cache . -t architechbootcamp/random-quote:${1:-latest}
